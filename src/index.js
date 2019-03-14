38db0545769d1e2c9e974575cb53416c2698211d
Dzianis Sheka <dzianis.sheka@gmail.com>
2019-03-14 23:59:07 +0300

    feat: implement first solution

--- src/index.js
+++ src/index.js
@@ -2,6 +2,40 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
+// minus 1
+// the next should be bigger then prev
+// exclude repeative
+// [2, 3, 1]
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  // console.log('preferences', preferences);
+
+  let count = 0;
+
+
+  preferences.forEach((person, index)=> {
+    const dontRepeat = [person - 1];
+    var nextLover = preferences[person - 1];
+    if (index < person || nextLover < person || dontRepeat.includes(nextLover-1)) {
+      // console.log('nextLover return', person, nextLover, dontRepeat);
+      return;
+    }
+    dontRepeat.push(nextLover-1);
+    var nextNextLover = preferences[nextLover - 1];
+    if (dontRepeat.includes(nextNextLover-1)) {
+      // console.log('nextNextLover return', nextLover, nextNextLover, dontRepeat);
+      return;
+    }
+    
+
+    const endOfTriangle = preferences[nextNextLover - 1];
+
+    // console.log('nextNextLover', index, person, nextLover, nextNextLover, endOfTriangle);
+
+    if (index === (nextNextLover - 1)) {
+      count++
+    }
+  });
+
+  // console.log('result', count);
+  return count;
 };
